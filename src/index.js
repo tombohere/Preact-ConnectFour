74be939be2cb1f44f631ce29aba8061d2012c59d
Tom <tom_here@yahoo.com>
2020-04-28 09:10:49 -0700

    commit #1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,176 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import redclickURL from './redclick.mmm';
+import yellowclickURL from './yellowclick.mmm';
+import redwinURL from './red.mmm';
+import yellowwinURL from './yellow.mmm';
+import nobodyURL from './nobody.mmm';
+
+const App = () => (
+  <div className="App">
+    <Board />
+  </div>
+);
+
+const Board = () => {
+  const [game, setGame] = useState(makeBoard());
+
+  const redclickPlay = new Audio(redclickURL);
+  const yellowclickPlay = new Audio(yellowclickURL);
+  const redwinPlay = new Audio(redwinURL);
+  const yellowwinPlay = new Audio(yellowwinURL);
+  const nobodyPlay = new Audio(nobodyURL);
+
+  function makeBoard() {
+    let board = new Array(7);
+    for (let i = 0; i < board.length; i++) {
+      board[i] = [];
+      for (let j = 0; j < 6; j++) {
+        board[i].push(0);
+      }
+    }
+    return {
+      board,
+      gameStarted: false,
+      player1: true,
+      count: 0,
+      gameOver: false,
+      playerWin: 0
+    };
+  }
+
+  const startGame = () => setGame({ ...game, gameStarted: true });
+
+  const restartGame = () => setGame({ ...makeBoard(), gameStarted: true });
+
+  const checkWin = () => {
+    let b = game.board;
+    let t;
+    for (let i = 0; i < 4; i++) {
+      for (let j = 5; j >= 0; j--) {
+        t = b[i][j] + b[i + 1][j] + b[i + 2][j] + b[i + 3][j];
+        if (t === 4 || t === 20) return true;
+      }
+    }
+    for (let j = 0; j < 3; j++) {
+      for (let i = 0; i < 7; i++) {
+        t = b[i][j] + b[i][j + 1] + b[i][j + 2] + b[i][j + 3];
+        if (t === 4 || t === 20) return true;
+      }
+    }
+    for (let j = 5; j > 1; j--) {
+      for (let i = 0; i < 4; i++) {
+        t = b[i][j] + b[i + 1][j - 1] + b[i + 2][j - 2] + b[i + 3][j - 3];
+        if (t === 4 || t === 20) return true;
+        t = b[6 - i][j] + b[5 - i][j - 1] + b[4 - i][j - 2] + b[3 - i][j - 3];
+        if (t === 4 || t === 20) return true;
+      }
+    }
+    return false;
+  };
+
+  const colClick = n => {
+    if (game.gameStarted && !game.gameOver) {
+      game.player1 ? redclickPlay.play() : yellowclickPlay.play();
+      let temp = { ...game };
+      for (let i = temp.board[n].length - 1; i >= 0; i--) {
+        if (temp.board[n][i] === 0) {
+          temp.board[n][i] = temp.player1 ? 1 : 5;
+          temp.count++;
+          if (checkWin()) {
+            temp.gameOver = true;
+            temp.playerWin = temp.player1 ? 1 : 2;
+            temp.player1 ? redwinPlay.play() : yellowwinPlay.play();
+          } else {
+            if (temp.count === 42) {
+              temp.gameOver = true;
+              nobodyPlay.play();
+            }
+            temp.player1 = !temp.player1;
+          }
+          setGame(temp);
+          break;
+        }
+      }
+      checkWin();
+    }
+  };
+
+  const playerColor = n =>
+    ({ 0: "", 1: " player1Color", 5: " player2Color" }[n]);
+
+  const circleClass = () =>
+    ({ true: " player1Color", false: " player2Color" }[game.player1]);
+
+  const backColor = () =>
+    ({ true: " AppRed", false: " AppYellow" }[game.player1]);
+
+  return (
+    <div className={"App" + backColor()}>
+      <div id="board">
+        <div className="board-container">
+          <div />
+          <div className="board-title-area">
+            <div />
+            <div className="board-position">
+              <div className={"board-circle top-circle" + circleClass()}>
+                GO
+              </div>
+            </div>
+            <div id="board-info">CONNECT 4</div>
+          </div>
+          <div />
+          <div className="board-game-area">
+            <div />
+            {[0, 1, 2, 3, 4, 5, 6].map(i => (
+              <div className="board-column" key={i} onClick={() => colClick(i)}>
+                <div className="board-column-container">
+                  {[0, 1, 2, 3, 4, 5].map(j => (
+                    <div className="board-position" key={j}>
+                      <div
+                        className={
+                          "board-circle" + playerColor(game.board[i][j])
+                        }
+                      >
+                        {""}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+            <div />
+          </div>
+        </div>
+      </div>
+      {!game.gameStarted ? (
+        <div className="board-message-container" onClick={startGame}>
+          <div className="board-message">START</div>
+        </div>
+      ) : (
+        ""
+      )}
+      {game.gameOver ? (
+        <div className="board-message-container" onClick={restartGame}>
+          <div className="board-message">
+            {game.player1
+              ? game.playerWin === 0
+                ? "NOBODY"
+                : "RED"
+              : game.playerWin === 0
+              ? "NOBODY"
+              : "YELLOW"}
+            <br />
+            WINS
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +178,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
